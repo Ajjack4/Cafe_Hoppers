d703c40c93c57076d8b317265bc9e216fa36f081
Ajjack4 <manavjadhav1997@gmail.com>
2024-08-11 21:59:40 +0530

    First commit

---
+++ client/src/App.tsx
@@ -0,0 +1,15 @@
+
+
+ 
+
+const App = () => {
+  return (
+    <div>
+       <div className="h-screen w-screen flex justify-center items-center bg-green-50">
+        Hello World
+     </div>
+    </div>
+  )
+}
+
+export default App
