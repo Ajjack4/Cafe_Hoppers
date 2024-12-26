53292cd18dbcef4bc273d926a0a61a8668cdc759
Ajjack4 <manavjadhav1997@gmail.com>
2024-12-26 23:21:12 +0530

    added mapbox

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,12 +1,18 @@
-
-
-import Login from '../components/User/Login'
-import Navbar from '../components/Navbar'
-import Notification from '../components/Notification'
-import Loading from '../components/Loading'
-import Map from 'react-map-gl';
+import Login from "../components/User/Login";
+import Navbar from "../components/Navbar";
+import Notification from "../components/Notification";
+import Loading from "../components/Loading";
+import Map, { Marker } from "react-map-gl";
+import { useState } from "react";
+// import Pin from "@mui/icons-material/LocationOnOutlined";
 
 const Home = () => {
+  const [viewState, setViewState] = useState({
+    longitude: 73.8567,
+    latitude: 18.5204,
+    zoom: 11.5,
+  });
+
   return (
     <div className="h-screen w-screen overflow-hidden">
       <Loading />
@@ -16,17 +22,17 @@ const Home = () => {
       <div className="h-full w-full">
         <Map
           mapboxAccessToken={import.meta.env.VITE_MAPBOX}
-          initialViewState={{
-            longitude: 73.8567,
-            latitude: 18.5204,
-            zoom: 11.5,
-          }}
-          style={{ width: "100%", height: "100%" }}
+          {...viewState}
+          onMove={(evt) => setViewState(evt.viewState)}
           mapStyle="mapbox://styles/mapbox/streets-v9"
-        />
+        >
+          <Marker longitude={73} latitude={18}>
+            <div>hello</div>
+          </Marker>
+        </Map>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
