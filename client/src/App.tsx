e89bd6b37223b2f68e027555b613ef6e61db07b1
Ajjack4 <manavjadhav1997@gmail.com>
2025-01-12 19:12:06 +0530

    nothing major just cleaned up  the code

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,20 +1,9 @@
 import Home from './pages/Home'
 import 'mapbox-gl/dist/mapbox-gl.css';
 
-// import {UPDATEUSER} from './slice/slice'
-// import { useDispatch } from 'react-redux';
-// import { RootState } from "./slice/stateStore";
-// import {  useSelector } from "react-redux";
+
 const App = () => {
-  // const user = useSelector((state: RootState) => state.user);
-  // const dispatch = useDispatch();
-  // const handlesubmit=()=>{
-   
-  //   dispatch(UPDATEUSER({
-  //     name:"test",
-  //     Login:true,
-  //   }))
-  // }
+
   return (
     <div>
 <Home/>
